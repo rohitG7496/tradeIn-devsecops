1310316c77c1d16840473f2a8b6ed27c4e4f701f
rohit <rogaikwad96@gmail.com>
2025-12-29 23:10:19 +0530

    fixes

--- Frontend/src/api/Request.js
+++ Frontend/src/api/Request.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_HOSTNAME = `http://${window.location.hostname}:8000/api/`;
+const API_HOSTNAME = "/api/";
 
 
 export const Request = async (method, endpoint, token, data) => {
@@ -9,12 +9,12 @@ export const Request = async (method, endpoint, token, data) => {
 		// timeout: 20000,
 		headers: token
 			? {
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${token}`,
-			  }
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			}
 			: {
-					"Content-Type": "application/json",
-			  },
+				"Content-Type": "application/json",
+			},
 		data,
 	}).catch((err) => err.response);
 };
