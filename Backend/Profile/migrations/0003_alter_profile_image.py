8351de759787e6cfa06f64b0fbd60af783951ce9
rohit <rogaikwad96@gmail.com>
2025-12-30 20:10:09 +0530

    fixes

--- Backend/Profile/migrations/0003_alter_profile_image.py
+++ Backend/Profile/migrations/0003_alter_profile_image.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('Profile', '0002_alter_profile_user_id'),
+        ('Profile', '0001_initial'),
     ]
 
     operations = [
